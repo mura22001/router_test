4947233a6e7a4ac31e03a28d4adb2e2c69b5f070
tatsuaki murahashi <tatsuaki.murahashi@infofarm.co.jp>
2020-05-21 11:39:50 +0900

    create router go to page3

---
+++ my-project/src/components/page1.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>
+      ここはページ１です。
+    </p>
+    <router-link to="/page2">Go to page2</router-link>
+    <router-link to="/page3">Go to page3</router-link>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ my-project/src/components/page3.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>
+      ここはページ3です。
+    </p>
+    <router-link to="/">Go to page1</router-link>
+    <router-link to="/page2">Go to page2</router-link>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
